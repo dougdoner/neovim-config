5b7872cb4830a5dbdc712e66adbc2758f068c76c
Doug Doner <ddoner02@gmail.com>
2023-01-06 11:26:50 -0800

    Removes reference to undodir

--- lua/dougdoner/set.lua
+++ lua/dougdoner/set.lua
@@ -13,7 +13,6 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
