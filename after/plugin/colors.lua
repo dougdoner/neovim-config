b2f926431e7427cad5d754dbe14c3ac86a25b768
dougdoner <ddoner02@gmail.com>
2023-01-30 15:16:19 -0800

    Adds clipboard option

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,9 @@ function SetColorScheme(color)
 	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+
 end
 
 SetColorScheme()
