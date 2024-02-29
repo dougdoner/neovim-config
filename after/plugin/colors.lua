965394667da56bd59e7d27b24600161c59799997
dougdoner <ddone02@gmail.com>
2024-02-29 11:00:17 -0800

    hopefully this fixes things

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,11 @@
 function SetColorScheme(color)
 
 
-	color = color or "tokyonight"
+	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+	-- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	-- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 
 end
 
